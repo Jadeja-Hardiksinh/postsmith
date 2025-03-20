6cea298755a1a35746f99a1a2bdec7c627280bdf
Jadeja Hardiksinh <jadejahardiksinh940@gmail.com>
2025-03-20 11:06:59 +0530

    adding project folders

---
+++ src/main/java/com/learn/postsmith/PostsmithApplication.java
@@ -0,0 +1,13 @@
+package com.learn.postsmith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostsmithApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostsmithApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/postsmith/PostsmithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.postsmith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostsmithApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
